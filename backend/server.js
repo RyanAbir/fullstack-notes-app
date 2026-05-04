9f44f2be7ff0d8ede62dccee6d6789a7f73456ac
Ryan Abir <info.ryanabir@gmail.com>
2026-05-04 17:55:18 +0000

    build basic fullstack app

---
+++ backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/message", (req, res) => {
+  res.json({ message: "Hello from backend API 🚀" });
+});
+
+app.listen(5000, () => {
+  console.log("Backend running on http://127.0.0.1:5000");
+});
